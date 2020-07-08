641756e3ccb698635a25521e58faa07a2b3ccbc2
Ian S <vallamost@gmail.com>
2020-07-07 20:52:01 -0700

    added some additional comments, adjusted null check.

--- problems/reverseAString.js
+++ problems/reverseAString.js
@@ -4,6 +4,7 @@
 
 // reverseStr("cat") => "tac"
 
+// access the string as a sorted array, start at the end
 //  0    1     2
 //["c", "a", "t"]
 
@@ -13,20 +14,22 @@
 
 function reverseStr(inputStr) {
 
-    if (inputStr.length <= 1 || inputStr === null || typeof(inputStr) === "number" ) {
-        // does not cover identical characters
-        console.log("Not a valid input, please provide a string.")
+    // skip empty input, skip input less than 1 char,  skip numbers
+    if (!inputStr || inputStr.length <= 1  || typeof(inputStr) === "number" ) {
+        
+        console.log("Not a valid input, please provide a string. e.g. \"cat\" ")
         return false;
     }
     
     // find the total length of the string to know where to start.
     startChar = inputStr.length
     for (letter in inputStr) {
-        console.log(inputStr[startChar - 1])
+        // because an array is zero based, we need to start one lower than the total length
+        console.log(inputStr[startChar -1])
         startChar -= 1;
     }
     return true;
 }
 
-reverseStr("cat") // => "tac", true
+// reverseStr("cat") // => "tac", true
 // reverseStr(11)  // => false
